b0328bda516aa86cd0eea3a04dacadd5ba58ec54
Sahil Gupta <155609546+2008sahil@users.noreply.github.com>
2024-02-24 23:00:58 +0530

    Update Backend.js

--- Backend.js
+++ Backend.js
@@ -11,12 +11,17 @@ const app = express();
 app.use(cors())
 
 app.use(express.json()); 
+
+
 const PORT = process.env.PORT;
 const server = app.listen(
-    PORT,
-    console.log(`Server running on PORT ${PORT}...`)
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`)
   );
   
+  app.get("/", (req, res) => {
+      res.send("API is running..");
+    });
   const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
